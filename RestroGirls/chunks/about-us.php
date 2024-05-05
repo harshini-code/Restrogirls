318773b49ac1ec6c6b09b5bf6a65e781c799c69f
harshini-code <149676245+harshini-code@users.noreply.github.com>
2024-05-05 17:41:23 +0530

    Initial commit

---
+++ RestroGirls/chunks/about-us.php
@@ -0,0 +1,19 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">Welcome to Restro Girls, your ultimate destination for an authentic Indian dining experience in the heart of Kolkata. Immerse yourself in the vibrant flavors, rich aromas, and warm hospitality of India as you embark on a culinary journey like no other.
+
+At Restro Girls, we take pride in showcasing the diverse and exquisite flavors of Indian cuisine. Our talented team of chefs skillfully craft each dish with a perfect blend of traditional techniques and modern innovation. From sizzling tandoori grills and aromatic curries to mouthwatering biryanis and delectable street food, our menu offers a wide range of options to tantalize your taste buds.
+
+Step into our beautifully designed restaurant, where the captivating ambiance and welcoming decor create an inviting atmosphere. Whether you're enjoying an intimate dinner, celebrating a special occasion, or hosting a corporate event, our attentive staff will ensure a memorable experience for you and your guests.
+
+We understand the importance of catering to different dietary preferences, which is why we offer a variety of vegetarian, vegan, and gluten-free options. Our commitment to using fresh, high-quality ingredients ensures that each dish is a true reflection of authentic Indian flavors.
+
+To complement your meal, we offer a selection of handcrafted cocktails, traditional Indian beverages, and a carefully curated wine list. Our knowledgeable staff is always ready to guide you through the menu, suggesting perfect pairings to enhance your dining experience.
+
+Whether you choose to dine in, takeout, or have your meal delivered, we strive to provide exceptional service and ensure your utmost satisfaction. We also offer catering services, bringing the flavors of India to your special events and gatherings.
+
+Join us at Restro Girls and let us transport you to the vibrant streets of India through our culinary creations. Book your table today and embark on a flavorsome journey that will leave you craving for more.</p>
+		      </div>
+	</section>
